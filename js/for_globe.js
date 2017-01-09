52a9c7ee064b980d3cb21676ce7deb030f9cb4c7
Miller <millerliang@gmail.com>
2017-01-09 11:51:29 +0800

    add aside icon $ css style

--- js/for_globe.js
+++ js/for_globe.js
@@ -11,31 +11,31 @@ jQuery(document).ready(function($){
 
 	// Left Aside Menu Links
 	var aside_text =[{
-			nav_text:"收支簿",nav_url:"p1.html"
+			nav_ico:"book",nav_text:"收支簿",nav_url:"p1.html"
 			},{
-			nav_text:"儲蓄目標設定",nav_url:"p6.html"
+			nav_ico:"bullseye",nav_text:"儲蓄目標設定",nav_url:"p6.html"
 			},{
-			nav_text:"支出預算設定",nav_url:"p17.html"
+			nav_ico:"dot-circle-o",nav_text:"支出預算設定",nav_url:"p17.html"
 			},{
-			nav_text:"分析-儲蓄目標",nav_url:"p10.html"
+			nav_ico:"line-chart",nav_text:"分析-儲蓄目標",nav_url:"p10.html"
 			},{
-			nav_text:"分析-支出預算",nav_url:"p9.html"
+			nav_ico:"bar-chart",nav_text:"分析-支出預算",nav_url:"p9.html"
 			},{
-			nav_text:"分析-3S",nav_url:"p12.html"
+			nav_ico:"area-chart",nav_text:"分析-3S",nav_url:"p12.html"
 			},{
-			nav_text:"分析-類別",nav_url:"p18.html"
+			nav_ico:"pie-chart",nav_text:"分析-類別",nav_url:"p18.html"
 			},{
-			nav_text:"我的心得",nav_url:"p16.html"
+			nav_ico:"wpforms",nav_text:"我的心得",nav_url:"p16.html"
 			},{
-			nav_text:"我的金幣",nav_url:"p15.html"
+			nav_ico:"database",nav_text:"我的金幣",nav_url:"p15.html"
 			},{
-			nav_text:"我的個資",nav_url:"p14.html"
+			nav_ico:"vcard-o",nav_text:"我的個資",nav_url:"p14.html"
 			}
 		];
 	$.each(aside_text,function(index,obj)
 	{
 		aside_items = "<li class='nav-primary'>";
-		aside_items += "<a href='" + obj.nav_url + "'><i class='fa fa-dot-circle-o'></i> " + obj.nav_text + "</a>";
+		aside_items += "<a href='" + obj.nav_url + "'><i class='fa fa-" + obj.nav_ico + "'></i> " + obj.nav_text + "</a>";
 		aside_items += "</li>";
 		$('#Nav_link').append(aside_items);
 	});
